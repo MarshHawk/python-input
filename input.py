97ef7c252f3cbc9055c9d8f4352a4711b2185b9d
SGlover3 <seanglover@rocketmortgage.com>
2022-03-08 14:11:38 -0500

    first commit

---
+++ input.py
@@ -0,0 +1,14 @@
+import random
+
+random.seed(0)
+
+ndarray = [[random.random() for _ in range(46)], [random.random()
+                                                      for _ in range(46)]]
+
+with open("assets/input-data.txt", "w") as f:
+    for _ in range(10000):
+        data = '{ "data": { ' \
+            + '"ndarray":' + '[[' + ', '.join(str(random.random()) for x in range(46)) \
+            + '],[' + ', '.join(str(random.random()) for x in range(46)) + ']]' \
+            + '}}\n'
+        f.write(data)
