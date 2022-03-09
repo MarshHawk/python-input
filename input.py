e1bca491cec5ce1ab7f8de2142e6fbde8a5b926e
SGlover3 <seanglover@rocketmortgage.com>
2022-03-08 22:47:32 -0500

    no absolute

--- input.py
+++ input.py
@@ -5,7 +5,7 @@ random.seed(0)
 ndarray = [[random.random() for _ in range(46)], [random.random()
                                                       for _ in range(46)]]
 
-with open("./assets/input-data.txt", "w") as f:
+with open("/assets/input-data.txt", "w") as f:
     for _ in range(10000):
         data = '{ "data": { ' \
             + '"ndarray":' + '[[' + ', '.join(str(random.random()) for x in range(46)) \
